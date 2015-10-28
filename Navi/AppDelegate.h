47ea4707cf20c4b65665f6ea987531fd55466ca0
fengwu2004 <user@users-imac-2.local>
2015-10-28 17:35:55 +0800

    Initial Commit

---
+++ Navi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Navi
+//
+//  Created by user on 15/10/28.
+//  Copyright © 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Navi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Navi
+//
+//  Created by user on 15/10/28.
+//  Copyright © 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
